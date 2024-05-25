764c1e509530b2b63381d1e60d910ccf98d44713
Abdul <abdulsalawu12@gmail.com>
2024-05-25 01:38:05 -0600

    completed home page layout including sidebar for all screen sizes

--- src/_root/Rootlayout.tsx
+++ src/_root/Rootlayout.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Bottombar from '@/components/shared/Bottombar'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+
+import { Outlet } from 'react-router-dom'
 
 const Rootlayout = () => {
   return (
-    <div>Rootlayout</div>
+    <div className='w-full md:flex'>
+      <Topbar />
+      <LeftSidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+      <Bottombar />
+
+    </div>
   )
 }
 
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
@@ -14,3 +14,8 @@ export const useSignInAccount = () => {
     })
 }
 
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
+    })
+}
