1494a60117cbb1ae038787770a36737393457e3d
Abdul <abdulsalawu12@gmail.com>
2024-05-27 04:41:47 -0600

    completed likes and saves funtionality for Post Card

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -16,9 +16,10 @@ import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
  
 type PostFormProps = {
     post?: Models.Document
+    action: 'Creat' | 'Update';
 }
 
-const PostForm = ({post}: PostFormProps) => {
+const PostForm = ({post, action}: PostFormProps) => {
     const {mutateAsync: createPost, isPending: isLoadingCreate} = useCreatePost();
     const { user } = useUserContext();
     const {toast} = useToast();
